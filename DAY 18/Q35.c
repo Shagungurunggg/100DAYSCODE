e23763d9f34fd1d2d516017a91c8d78e1cdfb2dc
Shagungurunggg <shagungurung03@gmail.com>
2025-11-04 16:45:19 +0530

    Q35.c

---
+++ DAY 18/Q35.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int factor = 1, n, i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("Error: Factors of a negative number doesn't exist.\n");
+        return 0;
+    }
+    for(i=1; i<=n; i++)
+    {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+        printf("Factors of %d is %d\n", n, i);
+    }
+    return 0;
+}
