b6a8a4fbe06c64418f616b8cbed211d89096ad5c
Shagungurunggg <shagungurung03@gmail.com>
2025-11-04 16:45:53 +0530

    q36.c

---
+++ DAY 18/q36.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int hcf, n1, n2, i;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &n1, &n2);
+    for(i=1; i <= n1 && i <= n2; i++) {
+        if(n1 % i == 0 && n2 % i == 0) {
+            hcf = i;
+        }
+    }
+    printf("HCF of %d and %d is %d\n", n1, n2, hcf);
+    return 0;
+}
