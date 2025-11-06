5ff50ccc83292b660f0aa9820c64d289c885c79f
Shagungurunggg <shagungurung03@gmail.com>
2025-11-06 21:25:44 +0530

    Q52.c

---
+++ DAY 26/Q52.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // First star
+    printf("*\n\n");
+
+    // 3 stars
+    for (int i = 1; i <= 3; i++)
+        printf("*\n");
+    printf("\n");
+
+    // 5 stars
+    for (int i = 1; i <= 5; i++)
+        printf("*\n");
+    printf("\n");
+
+    // 3 stars
+    for (int i = 1; i <= 3; i++)
+        printf("*\n");
+    printf("\n");
+
+    // Last star
+    printf("*\n");
+
+    return 0;
+}
