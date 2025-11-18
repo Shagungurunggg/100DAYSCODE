1955f2e9be4e47297c59edff759231e6d61685ee
Shagungurunggg <shagungurung03@gmail.com>
2025-11-18 20:26:56 +0530

    q81.c

---
+++ DAY 41/q81.c
@@ -0,0 +1,16 @@
+Count characters in a string without using built-in length functions.
+#include <stdio.h> 
+int main()
+{
+    char str[100];
+    int count_str = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    while (str[count_str] != '\0')
+    {
+        count_str++;
+    }
+    printf("Length of the string: %d\n", count_str - 1); 
+    return 0;
+}   
+    
