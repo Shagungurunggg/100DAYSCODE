17c6a3b70904a79848b78a0463b2cb3e23f14ced
Shagungurunggg <shagungurung03@gmail.com>
2025-12-01 22:33:33 +0530

    q138.c

---
+++ DAY 88/q138.c
@@ -0,0 +1,12 @@
+Print all enum names and integer values using a loop.
+#include <stdio.h>
+
+int main() {
+    enum Colors {RED, YELLOW, GREEN};
+    const char* names[] = {"RED", "YELLOW", "GREEN"};
+
+    for (int i = 0; i <= GREEN; i++)
+        printf("%s=%d\n", names[i], i);
+
+    return 0;
+}
